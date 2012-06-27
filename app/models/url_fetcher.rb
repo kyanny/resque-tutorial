6f29142bef1bb98f393f8ab73151161742861d49
Kensuke Nagae <kyanny@gmail.com>
2012-06-27 20:22:34 +0900

    fix

--- app/models/url_fetcher.rb
+++ app/models/url_fetcher.rb
@@ -2,7 +2,7 @@ class UrlFetcher
   @queue = :fetch_title
   def self.perform(bookmark_id, url)
     html = Kernel.open(url).read
-    title = (html.scan(/<title>(.*?)<\/title>/i).flatten)[1]
+    title = (html.scan(/<title>(.*?)<\/title>/i).flatten).first
     bookmark = Bookmark.find(bookmark_id)
     bookmark.title = NKF.nkf('-m0 -w', title)
     bookmark.save
