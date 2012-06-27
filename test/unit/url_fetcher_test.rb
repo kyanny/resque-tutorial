53f758b554769bda178720088e50353f6fb57d53
Kensuke Nagae <kyanny@gmail.com>
2012-06-27 20:17:20 +0900

    Add test

---
+++ test/unit/url_fetcher_test.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+require 'test_helper'
+
+class UrlFetcherTest < ActiveSupport::TestCase
+  setup do
+    @bookmark = Bookmark.create
+  end
+
+  test 'perform' do
+    io = stub('io', read: '<html><head><title>ライブドア</title></head><body>ライブドアのページだよー</body></html>')
+    Kernel.stubs(:open).returns(io)
+    UrlFetcher.perform(@bookmark.id, 'http://www.livedoor.com/')
+    assert true
+  end
+end
+
+
+
+
+
+
+
