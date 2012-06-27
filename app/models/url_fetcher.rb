981aa5aad613f80d5e620f0a5f427e9767be64d1
Kensuke Nagae <kyanny@gmail.com>
2012-06-27 20:07:05 +0900

    case insensitive match

--- app/models/url_fetcher.rb
+++ app/models/url_fetcher.rb
@@ -2,7 +2,7 @@ class UrlFetcher
   @queue = :fetch_title
   def self.perform(bookmark_id, url)
     open(url){ |f|
-      title = (f.scan(/<title>(.*?)<\/title>/).flatten)[1]
+      title = (f.scan(/<title>(.*?)<\/title>/i).flatten)[1]
       bookmark = Bookmark.find(bookmark_id)
       bookmark.title = title
       bookmark.save
