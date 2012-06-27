28866e399b8dfb8e3832273bac6f8218311c130c
Kensuke Nagae <kyanny@gmail.com>
2012-06-27 19:48:15 +0900

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UmaiBookmark::Application.config.secret_token = 'f08c7ad1fe889634a42f5482f7a4d5f23fd99c66f85364dbdd451692d14abbd29c60237bd5c330cb6c98b8f9a0975bc559638f741f19a5a085e14e4ab323915e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UmaiBookmark::Application.config.session_store :cookie_store, key: '_UmaiBookmark_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UmaiBookmark::Application.config.session_store :active_record_store
