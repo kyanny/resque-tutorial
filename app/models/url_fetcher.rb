f80c3f51536819da29a3cb3dc8d3ad468ff41008
Kensuke Nagae <kyanny@gmail.com>
2012-06-27 20:18:01 +0900

    refactoring

--- app/models/url_fetcher.rb
+++ app/models/url_fetcher.rb
@@ -1,11 +1,10 @@
 class UrlFetcher
   @queue = :fetch_title
   def self.perform(bookmark_id, url)
-    open(url){ |f|
-      title = (f.scan(/<title>(.*?)<\/title>/i).flatten)[1]
-      bookmark = Bookmark.find(bookmark_id)
-      bookmark.title = NKF.nkf('-m0 -w', title)
-      bookmark.save
-    }
+    html = open(url).read
+    title = (html.scan(/<title>(.*?)<\/title>/i).flatten)[1]
+    bookmark = Bookmark.find(bookmark_id)
+    bookmark.title = NKF.nkf('-m0 -w', title)
+    bookmark.save
   end
 end
